031736c8e220ed2de31fe7618afec9e490627c20
Muath Hassoun <moath.hassoun@gmail.com>
2025-09-19 21:07:17 +0300

    Download Button Updated!

--- public/js/app-version.js
+++ public/js/app-version.js
@@ -8,7 +8,18 @@ fetch("https://api.github.com/repos/MuathHassoun/battorion-version/releases/late
     if (versionBtn) {
       versionBtn.innerHTML = `${versionText}`;
     }
-  })
-  .catch(error => {
+
+    const mainAsset = data.assets.find(asset => asset.name.includes("_battorion") && asset.name.endsWith(".exe"));
+    let sizeText = "Size: N/A";
+    if (mainAsset) {
+      const sizeMB = (mainAsset.size / (1024 * 1024)).toFixed(2);
+      sizeText = `Size: ${sizeMB} MB`;
+    }
+
+    const sizeTextSpan = document.getElementById("size-text");
+    if (sizeTextSpan) {
+      sizeTextSpan.innerHTML = `${sizeText}`;
+    }
+  }).catch(error => {
     console.error("Failed to fetch version info:", error);
   });
